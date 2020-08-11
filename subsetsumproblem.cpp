5fa42563066c78cc7b8a2b7e01882a8cb339f93e
Nivedita kumari <44724151+niveditaprity@users.noreply.github.com>
2020-08-11 09:36:40 +0530

    Update subsetsumproblem.cpp

--- subsetsumproblem.cpp
+++ subsetsumproblem.cpp
@@ -1,59 +1,68 @@
-#include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
+bool issubsetsum(int n,int arr[],int sum)
+{
+  if(sum%2==1)
+  {
+      return false;
+  }
+  int target=sum/2;
+        bool t[n+1][target+1];
+        for(int i=0;i<n+1;i++)
+        {
+            for(int j=0;j<target+1;j++)
+            {
+                if(i==0)
+                {
+                    t[i][j]=false;
+                }
+                if(j==0)
+                {
+                    t[i][j]=true;
+                }
+            }
+        }
+        for(int i=1;i<n+1;i++)
+        {
+            for(int j=1;j<target+1;j++)
+            {
+                if(arr[i-1]<=j)
+                {
+                  t[i][j]=t[i-1][j]||t[i-1][j-arr[i-1]];  
+                }
+                else
+                {
+                    t[i][j]=t[i-1][j];
+                }
+            }
+        }
+        return t[n][target];
+}
 int main()
  {
-     int t;
-     cin>>t;
-     while(t--)
-     {
-         int n;
-         cin>>n;
-         int arr[n];
-         int sum=0;
-         for(int i=0;i<n;i++)
-         {
-             cin>>arr[i];
-             sum+=arr[i];
-         }
-         sum=sum/2;
-         bool t[n+1][sum+1];
-         for(int i=0;i<n+1;i++)
-         {
-             for(int j=0;j<sum+1;j++)
-             {
-                 if(j==0)
-                 {
-                     t[i][j]=true;
-                 }
-                 if(i==0)
-                 {
-                     t[i][j]=false;
-                 }
-             }
-         }
-         for(int i=1;i<n+1;i++)
-         {
-             for(int j=1;j<sum+1;j++)
-             {
-              if(arr[i]<=j)
-              {
-                  t[i][j]=t[i][j-arr[i-1]]||t[i-1][j];
-              }
-              else
-              {
-                  t[i][j]=t[i-1][j];
-              }
-             }
-         }
-         if(t[n][sum]==true)
-         {
-             cout<<"YES"<<endl;
-         }
-         else
-         {
-             cout<<"NO"<<endl;
-         }
-         
-     }	//code
+int t;
+cin>>t;
+while(t--)
+{
+    int n;
+    cin>>n;
+    int arr[n];
+    int sum=0;
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+        sum+=arr[i];
+        
+    }
+    if(issubsetsum(n,arr,sum)==true)
+    {
+        cout<<"YES"<<endl;
+    }
+    else
+    {
+        cout<<"NO"<<endl;
+    }
+}
+    //code
 	return 0;
 }
