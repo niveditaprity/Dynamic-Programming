31467637386a994888288f662eac6666d2b0e2b5
Nivedita kumari <44724151+niveditaprity@users.noreply.github.com>
2020-08-17 13:01:44 +0530

    Create longest_common_subsequences.cpp

---
+++ longest_common_subsequences.cpp
@@ -0,0 +1,14 @@
+int lcs(int x, int y, string s1, string s2){
+   if(x==0||y==0)
+   {
+       return 0;
+   }
+   if(s1[x-1]==s2[y-1])
+   {
+       return 1+lcs(x-1,y-1,s1,s2);
+   }
+   else
+   {
+      return  max(lcs(x-1,y,s1,s2),lcs(x,y-1,s1,s2));
+   }
+   //Recursive (TLE)
